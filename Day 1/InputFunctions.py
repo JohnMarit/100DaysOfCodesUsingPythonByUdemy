6c58661444e287d5b6336ad2209a5e9eb92ba8e2
JohnMarit <thuchabraham42@gmail.com>
2023-11-21 10:14:14 +0300

    Day 1

---
+++ Day 1/InputFunctions.py
@@ -0,0 +1,18 @@
+#print (6) when the input for num1= 2 and num2= 3
+
+#initial code
+"""
+num1 = int(input())
+num2 = int(input())
+"""
+# submitted code
+num1 = int(input())
+num2 = int(input())
+print(num1*num2)
+
+
+# another exercise
+# printing the length of the name that is being used for input
+
+#Input name is Angela
+print(len(input()))
