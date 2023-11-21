c1667eb22bdfd1d980ae744ce7f7b13cfd19d791
JohnMarit <thuchabraham42@gmail.com>
2023-11-21 23:29:01 +0300

    Day 1

---
+++ Day 1/variables.py
@@ -0,0 +1,17 @@
+# There are two variables, a and b from input
+a = input()
+b = input()
+# 🚨 Don't change the code above ☝️
+####################################
+# Write your code below this line 👇
+#the solution was to switch in between values
+
+c = a
+a = b 
+b = c
+
+
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
