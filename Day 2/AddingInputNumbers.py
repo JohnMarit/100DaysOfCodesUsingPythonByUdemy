bfe5f8d1de6dc223444dd7cf0b82d9d8bab682a7
JohnMarit <thuchabraham42@gmail.com>
2023-11-22 11:02:20 +0300

    Day 2

---
+++ Day 2/AddingInputNumbers.py
@@ -0,0 +1,6 @@
+two_digit_number = input()
+# 🚨 Don't change the code above 👆
+####################################
+# Write your code below this line 👇
+
+print(int(two_digit_number[0]) + int(two_digit_number[1]))
