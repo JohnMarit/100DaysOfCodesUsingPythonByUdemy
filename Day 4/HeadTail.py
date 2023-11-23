1a7cf65bcdc4f61e03e833bb9578cf2dc3feb161
JohnMarit <thuchabraham42@gmail.com>
2023-11-23 22:56:32 +0300

    Day 4

---
+++ Day 4/HeadTail.py
@@ -0,0 +1,9 @@
+# Write your code below this line 👇
+# Hint: Remember to import the random module first. 🎲
+
+import random
+random_side = random.randint(0, 1)
+if random_side == 1:
+  print("Heads")
+else:
+  print("Tails")
