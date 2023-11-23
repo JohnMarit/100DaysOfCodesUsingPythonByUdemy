f16467b4e56c59b4f54b184cc56b6ec1a3e7237f
JohnMarit <thuchabraham42@gmail.com>
2023-11-23 23:42:44 +0300

    Day 3

---
+++ Day 4/BankerRoullette.py
@@ -0,0 +1,17 @@
+
+names_string = ("Angela, Ben, Jenny, Michael, Chloe ")
+names = names_string.split(", ")
+# The code above converts the input into an array seperating
+#each name in the input by a comma and space.
+# 🚨 Don't change the code above 👆
+
+import random
+
+num_items = len(names)
+random_choice = random.randint(0, num_items - 1)
+
+person_who_will_pay = names[random_choice]
+
+print(person_who_will_pay + " is going to buy the meal today!")
+
+
