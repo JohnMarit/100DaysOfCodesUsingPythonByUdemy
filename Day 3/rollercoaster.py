014b635474d384e48442c3b45ee56f0b98286198
JohnMarit <thuchabraham42@gmail.com>
2023-11-23 10:55:30 +0300

    Day 3

--- Day 3/rollercoaster.py
+++ Day 3/rollercoaster.py
@@ -3,5 +3,12 @@ height = int(input("What is your height in cm? "))
 
 if height > 120:
     print("You can ride the rollercoaster!")
+    age = int(input(f"What is your age? "))
+    if age < 12:
+      print("Please pay $5")
+    elif age <=18:
+      print("Please pay $7")
+    else:
+        print("Please pay $12")
 else:
     print("sorry, you have to grow tall to ride the rollercoaster.")
