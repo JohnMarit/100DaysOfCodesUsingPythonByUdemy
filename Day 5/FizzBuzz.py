985d1270a0f453a9d0bd72e0c7b12a56dfaa1e57
JohnMarit <thuchabraham42@gmail.com>
2023-11-24 17:05:17 +0300

    Day 3

---
+++ Day 5/FizzBuzz.py
@@ -0,0 +1,11 @@
+# Write your code here 👇
+target = 100
+for number in range(1, target + 1):
+  if number % 3 == 0 and number % 5 == 0:
+    print("FizzBuzz")
+  elif number % 3 == 0:
+    print("Fizz")
+  elif number % 5 == 0:
+    print("Buzz")
+  else:
+    print(number)
